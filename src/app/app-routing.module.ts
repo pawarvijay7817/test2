19a293322a971afedbb30810d480f0b2e4155e40
Pawar Vijay <pawarvijay7817@gmail.com>
2021-09-06 13:03:25 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+import { AuthGuard } from '../app/auth.guard';
+
+const routes: Routes = [
+  {path:'',component:LoginComponent},
+  {path:'login',component:LoginComponent},
+  { path: 'blogs', canActivate:[AuthGuard], 
+    loadChildren: () => import('./blogs/blogs.module').then(m => m.BlogsModule) 
+  },
+]; 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
